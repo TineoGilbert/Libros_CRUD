89ae96660d570fe1e93fb4cdf891ac5cd2891df1
TineoGilbert <65523573+TineoGilbert@users.noreply.github.com>
2021-06-24 11:47:16 -0400

    CRUD LIBROS

---
+++ Libros/Controllers/LibrosController.cs
@@ -0,0 +1,130 @@
+﻿using Libros.Data;
+using Libros.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Libros.Controllers
+{
+    public class LibrosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LibrosController( ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Libro> ListaLibros = _context.Libro;
+            return View(ListaLibros);
+        }
+
+
+        public IActionResult Create()
+        {
+           
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Libro libro)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Libro.Add(libro);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "Inserpción Exitosa";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+
+        public IActionResult Edit( int? id)
+        {
+
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var libro = _context.Libro.Find(id);
+
+            if (libro == null) 
+            {
+                return NotFound();
+            }
+
+            return View(libro);
+        }
+
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Libro.Update(libro);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "Actualización Completada!";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var libro = _context.Libro.Find(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return View(libro);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteLibro(int? id)
+        {
+
+            var libro = _context.Libro.Find(id);
+
+            if(libro == null)
+            {
+                NotFound();
+            }
+
+                _context.Libro.Remove(libro);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "El Libro se ha Eiminado!";
+                return RedirectToAction("Index");
+      
+        }
+
+
+
+    }
+}
---
+++ Libros/Models/Libro.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Libros.Models
+{
+    public class Libro
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El Título es obligatorio")]
+        [StringLength(50, ErrorMessage ="El {0} debe ser almenos {2} y máximo {1} Carácteres", MinimumLength = 3)]
+        [Display(Name = "Título")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "La Descripción es obligatoria")]
+        [StringLength(50, ErrorMessage = "El {0} debe ser almenos {2} y máximo {1} Carácteres", MinimumLength = 3)]
+        [Display(Name = "Descripción")]
+        public string Descripción { get; set; }
+
+        [Required(ErrorMessage = "El Fecha es obligatoria")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha Lanzamiento")]
+        public DateTime FechaLanzamiento { get; set; }
+
+        [Required(ErrorMessage = "El Autor es obligatorio")]
+        public string Autor { get; set; }
+
+        [Required(ErrorMessage = "El Precio es obligatorio")]
+        public int Precio { get; set; }
+    }
+}
